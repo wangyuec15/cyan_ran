939a56c33f1b4f53af53a96a727bcaa0598c1814
yuechenw213 <yuechenw@andrew.cmu.edu>
2023-06-13 11:34:30 -0400

    try wave effect

---
+++ public/sketch.js
@@ -0,0 +1,80 @@
+let positions = []; // Array to store previous mouse positions
+const delay = 50; // Number of frames of delay
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // underwave()
+  positions.push(0);
+}
+
+function draw() {
+  background(255);
+  wave();
+}
+
+function underwave() {
+  noStroke();
+  const purpleColor = color(148, 0, 211); // Purple color
+  purpleColor.setAlpha(100); // Set the alpha value for transparency
+
+  const amplitude = height / 16;
+  const frequency = 0.01;
+
+  for (let x = 0; x <= width; x += 10) {
+    for(let y = height + amplitude; y < height; y += 10) {
+      fill(purpleColor);
+      ellipse(x, y, 4, 4);
+    }
+  }
+}
+
+function wave() {
+  // Store current mouse position
+  const maxmove = 80;
+  if(abs(mouseX - positions[0]) > maxmove) {
+    if(mouseX > positions[0]) {
+      positions.push(positions[0] + maxmove);
+    } else {
+      positions.push(positions[0] - maxmove);
+    }
+    
+  }
+  // else {
+  //   positions.push( mouseX );
+  // }
+  
+  
+  // Remove oldest position if array length exceeds delay
+  if (positions.length > delay) {
+    positions.shift();
+  }
+  
+  // Draw delayed positions
+  // for (let i = 0; i < positions.length; i++) {
+  //   let pos = positions[i];
+  //   let alpha = map(i, 0, positions.length - 1, 0, 255);
+  //   fill(0, alpha);
+  //   ellipse(pos, 100, 20, 20);
+  // }
+  let pos = positions[0];
+  // ellipse(pos, 200, 20, 20);
+
+  noStroke();
+  const purpleColor = color(148, 0, 211); // Purple color
+  purpleColor.setAlpha(100); // Set the alpha value for transparency
+
+  const amplitude = height / 16;
+  const delta = height / 32;
+  const frequency = 0.006;
+
+  for (let x = 0; x <= width; x += 10) {
+    for (let y = height / 4 - amplitude - delta; y < height + amplitude + delta; y += 10) {
+      const waveY = height / 4 - (1 - abs(pos  - x ) / width)  *   amplitude * cos((x - pos) * frequency) * abs(cos(frameCount * 0.02)) ;
+      if (y >= waveY) {
+        fill(purpleColor);
+        ellipse(x, y, 4, 4);
+      }
+    }
+  }
+
+}
